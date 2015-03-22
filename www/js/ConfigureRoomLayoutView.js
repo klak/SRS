1ca2bfb39ad24cb3c638bb4085798946d996d361
dmklein <daniel.klein.dmk@gmail.com>
2015-03-21 22:06:10 -0500

    stuff is  starting to work again.

--- www/js/ConfigureRoomLayoutView.js
+++ www/js/ConfigureRoomLayoutView.js
@@ -1,4 +1,49 @@
 var ConfigureRoomLayoutView = function () {
+    
+    this.grid = function() {
+        var counter = 0;
+        console.log("in grid()...");
+        // Grabs the number from the form in the height box
+        //var height = document.getElementById("heightInput").value;
+        var height = $("#heightInput", this.$el).val();
+        console.log("height entered is: ", height);
+
+        //Grabs the number from the form in the width box
+        //var width = document.getElementById("widthInput").value;
+        var width = $("#widthInput", this.$el).val();
+        console.log("width entered is: ", width);
+        
+        var lastClicked;
+
+        var grid = clickableGrid(height, width,
+                    function(el, row, col, i)
+                    {
+                        console.log("You clicked on element:", el);
+                        console.log("You clicked on row:", row);
+                        console.log("You clicked on col:", col);
+                        console.log("You clicked on item #:", i);
+                  
+
+                        // Uses the class name to determine if the box has been clicked
+                  
+                        if (el.className == '') {
+                            el.className = 'clicked1';
+                        } else if (el.className == 'clicked1') {
+                            el.className = 'clicked2';
+                        } else if (el.className == 'clicked2') {
+                            el.className = '';
+                        }
+                    }
+        );
+
+        //prints the array
+        //var testGrid = document.body.appendChild(grid);
+        $("#test", this.$el).html(grid);
+
+        localStorage.setItem('gridHeight', height);
+        localStorage.setItem('gridWidth', width);
+
+    };
 
 	this.initialize = function() {
 		// div wrapper for view, used to attach events
@@ -14,6 +59,11 @@ var ConfigureRoomLayoutView = function () {
                 $("#wrapper", this.$el).toggleClass("toggled");
             });
 
+        // button for generating grid from coords
+        this.$el.on('click', '#generate-grid', this.grid);
+
+        // button for generating grid from coords
+        this.$el.on('click', '#save-grid', this.save);
 
 		this.render();
 	};
@@ -24,14 +74,71 @@ var ConfigureRoomLayoutView = function () {
         return this;
     };
 
-    /*this.findByName = function() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-            employeeListView.setEmployees(employees);
-        });
-    };*/
+
 
     this.initialize();
 
     // TODO: stick javascript that configure room needs in here somewhere.
     // TODO: build some sort of structure around local storage???
+}
+
+var clickableGrid = function(rows, cols, callback) 
+{
+    var i = 0;
+    var grid = document.createElement('table');
+    grid.className = 'grid';
+    grid.id = 'table';
+
+    //creates the rows
+    for (var r = 0; r < rows; ++r)
+    {
+        var tr = grid.appendChild(document.createElement('tr'));
+      
+        //creates the columns
+        for (var c = 0; c < cols; ++c)
+        {
+            var cell = tr.appendChild(document.createElement('td'));
+            //adds numbers to the cells
+            //cell.innerHTML = ++i;
+            cell.id = ++i;
+
+            cell.addEventListener('click', 
+                (function(el, r, c, i) {
+                    return function() {
+                        callback(el,r,c,i);
+                    };
+                })
+
+            (cell,r,c,i),false);
+        }
+    }
+    
+    return grid;
+};
+
+function save()
+{
+    var gridArray1 = new Array();
+    var gridArray2 = new Array();
+    var table = document.getElementById('table');
+    
+    for (var r = 0, n = table.rows.length; r < n; r++) 
+    {
+        for (var c = 0, m = table.rows[r].cells.length; c < m; c++) {
+            console.log(table.rows[r].cells[c].className);
+            if(table.rows[r].cells[c].className == 'clicked1'){
+              gridArray1.push(table.rows[r].cells[c].id);
+
+            }
+            else if(table.rows[r].cells[c].className == 'clicked2'){
+              gridArray2.push(table.rows[r].cells[c].id);
+            }
+        }
+    } 
+    var toLocal1 = gridArray1.toString();
+    var toLocal2 = gridArray2.toString(); 
+    console.log(toLocal1);
+    console.log(toLocal2);
+    localStorage.setItem('empChairArray', toLocal1);
+    localStorage.setItem('occChairArray', toLocal2);
 }
--- www/js/ViewRoomView.js
+++ www/js/ViewRoomView.js
@@ -17,11 +17,14 @@ var ViewRoomView = function () {
             });
 
 
+
+
 		this.render();
 	};
 
 	this.render = function() {
         this.$el.html(this.template());
+        this.grid();
         //$('.content', this.$el).html(employeeListView.$el);
         return this;
     };
@@ -32,8 +35,79 @@ var ViewRoomView = function () {
         });
     };*/
 
+    this.grid = function() {
+
+        var counter = 0;
+        // Grabs the number from the form in the height box
+        var height = localStorage.getItem('gridHeight');
+        console.log("height is: ", height);
+
+        //Grabs the number from the form in the width box
+        var width = localStorage.getItem('gridWidth');
+        console.log("width is: ", width);
+
+        var lastClicked;
+
+        var grid = clickableGrid(height, width,
+                    function(el,row,col,i){
+
+
+              });
+
+        //prints the array
+        //var testGrid = document.getElementById("test").appendChild(grid);
+        $("#test", this.$el).html(grid);
+
+
+    }
+
     this.initialize();
 
     // TODO: stick javascript that configure room needs in here somewhere.
     // TODO: build some sort of structure around local storage???
 }
+
+      var clickableGrid = function( rows, cols, callback) {
+          var i=0;
+          console.log("empty chairs are: ", localStorage.getItem('empChairArray'));
+          console.log("empty chairs are: ", localStorage.getItem('occChairArray'));
+
+          var empChairArray = localStorage.getItem('empChairArray').split(",");
+          var occChairArray = localStorage.getItem('occChairArray').split(",");
+          var grid = document.createElement('table');
+          grid.className = 'grid';
+
+          //creates the rows
+          for (var r=0;r<rows;++r){
+              var tr = grid.appendChild(document.createElement('tr'));
+              
+              //creates the columns
+              for (var c=0;c<cols;++c){
+                  var cell = tr.appendChild(document.createElement('td'));
+                  //adds numbers to the cells
+                  //cell.innerHTML = ++i;
+                  cell.id = ++i;
+                  if(cell.id == empChairArray[0]){
+                    cell.className = 'clicked1';
+                    empChairArray.shift();
+                  }
+
+                  else if(cell.id == occChairArray[0]){
+                    cell.className = 'clicked2';
+                    occChairArray.shift();
+                  }
+
+
+                  cell.addEventListener('click',(function(el,r,c,i){
+                      return function(){
+                          callback(el,r,c,i);
+                      };
+                  })
+
+                  (cell,r,c,i),false);
+              }
+          }
+          return grid;
+      }
+
+

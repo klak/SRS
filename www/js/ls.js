2ced9d21ebb9f5717880e030a5a9eecb372801e3
bmlaird <bmlaird@crimson.ua.edu>
2015-04-21 21:50:40 -0500

    Nothing

--- www/js/ls.js
+++ www/js/ls.js
@@ -3,7 +3,7 @@ function login(user, password){
 	var shaObj = new jsSHA(password+"uWY6tf87eEqddV2", "TEXT");
 	var hash = shaObj.getHash("SHA-512","HEX");
 	getDocsWithQuery("wwystest", "login_dev", JSON.stringify({"password":hash}))
-	.success(
+	.then(
 		function(loginInfo) {
 			if (loginInfo.length > 0 && user == loginInfo[0].username) {
 				validLogin = true;
